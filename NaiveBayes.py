e117aa36701142e342cd8e5d1ffbcd21b41cb354
Abhishek <abhishekasdzxc@gmail.com>
2019-08-05 15:36:18 +0530

    Update NaiveBayes.py

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,14 +1,37 @@
-
-# coding: utf-8
-
-# In[1]:
-
 import os
 import numpy as np
 import re
 
+class MultinomialNaiveBayes:
+    def __init__(self, alpha=1.0):
+        self.alpha = alpha # Used for laplace smoothing the features
+        self.class_prior = None # Prior Probabilities
+        self.fit_prior = True # Fit Prior
 
-# In[2]:
+    def _update_feature_log_prob(self,alpha):
+        smoothed_fc = self.feature_count_ + alpha
+        smoothed_cc = smoothed_fc.sum(axis=1).reshape(-1, 1)
+        self.feature_log_prob_ = (np.log(smoothed_fc) - np.log(smoothed_cc))
+
+    def _update_class_log_prior(self,class_prior=None):
+        log_class_count = np.log(self.class_count_)
+        self.class_log_prior_ = (log_class_count - np.log(self.class_count_.sum()))
+
+    def predict(self, X):
+        preds = ((X @ self.feature_log_prob_.T) + self.class_log_prior_)   #Joint log likelihood
+        return self.classes_[np.argmax(preds, axis=1)]
+
+    def fit(self, X, y):
+        _, n_features = X.shape
+        Y = np.reshape(y,(-1,1)).astype(np.float32)
+        self.classes_ = np.unique(Y)
+        Y = np.concatenate((1 - Y, Y), axis=1)
+        n_effective_classes = Y.shape[1]
+        self.feature_count_ = Y.T@X
+        self.class_count_ = Y.sum(axis=0)
+        self._update_feature_log_prob(self.alpha)
+        self._update_class_log_prior(class_prior=self.class_prior)
+        return self
 
 # Creates dictionary from all the emails in the directory
 def build_dictionary(dir):
@@ -57,82 +80,25 @@ def build_labels(dir):
         labels_matrix[index] = 1 if re.search('spms*', email) else 0
     return labels_matrix
 
-
-# In[3]:
-
 train_dir = './train_data'
 print('1. Building the dictionary')
 dictionary = build_dictionary(train_dir)
-
-
-# In[4]:
 
 print('2. Building the training features and labels')
 features_train = build_features(train_dir, dictionary)
 labels_train = build_labels(train_dir)
 
-
-# In[143]:
-
-class MultinomialNaiveBayes:
-    def __init__(self, alpha=1.0):
-        self.alpha = alpha # Used for laplace smoothing the features
-        self.class_prior = None # Prior Probabilities
-        self.fit_prior = True # Fit Prior
-        
-    def _update_feature_log_prob(self,alpha):
-        smoothed_fc = self.feature_count_ + alpha
-        smoothed_cc = smoothed_fc.sum(axis=1).reshape(-1, 1)
-        self.feature_log_prob_ = (np.log(smoothed_fc) - np.log(smoothed_cc))
-    
-    def _update_class_log_prior(self,class_prior=None):
-        log_class_count = np.log(self.class_count_)
-        self.class_log_prior_ = (log_class_count - np.log(self.class_count_.sum()))
-    
-    def predict(self, X):
-        preds = ((X @ self.feature_log_prob_.T) + self.class_log_prior_)   #Joint log likelihood
-        return self.classes_[np.argmax(preds, axis=1)]
-    
-    def fit(self, X, y):
-        _, n_features = X.shape
-        Y = np.reshape(y,(-1,1)).astype(np.float32)
-        self.classes_ = np.unique(Y)
-        Y = np.concatenate((1 - Y, Y), axis=1)
-        n_effective_classes = Y.shape[1]
-        self.feature_count_ = Y.T@X
-        self.class_count_ = Y.sum(axis=0)
-        self._update_feature_log_prob(self.alpha)
-        self._update_class_log_prior(class_prior=self.class_prior)
-        return self
-
-
-# In[144]:
-
 mnb = MultinomialNaiveBayes()
+print('3. Training the classifier')
 mnb.fit(features_train,labels_train)
+accuracytrain = (mnb.predict(features_train)==labels_train).mean()
+print(f'Train Accuracy : {accuracytrain*100:2.4f}%')
 
-
-# In[145]:
-
-Preds = mnb.predict(features_train)
-
-
-# In[146]:
-
-(Preds==labels_train).mean()
-
-
-# In[147]:
-
-# test_dir = './test_data'
-# print('4. Building the test features and labels')
-# features_test = build_features(test_dir, dictionary)
-# labels_test = build_labels(test_dir)
-
-
-# In[151]:
+test_dir = './test_data'
+print('4. Building the test features and labels')
+features_test = build_features(test_dir, dictionary)
+labels_test = build_labels(test_dir)
 
 print('5. Calculating the accuracy of the trained classifier over the test emails')
-accuracy = (mnb.predict(features_test)==labels_test).mean()
-print(f'\tAccuracy : {accuracy*100:2.4f}%')
-
+accuracytest = (mnb.predict(features_test)==labels_test).mean()
+print(f'Test Accuracy : {accuracytest*100:2.4f}%')
